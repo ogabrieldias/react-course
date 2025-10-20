3b333a1bf21bae7fccb4597e51e20ffc19408db7
ogabrieldias <gabrielbielbernardino@gmail.com>
2025-10-20 13:46:43 -0300

    Update Condicional | OutraLista

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
 import './App.css'
-import HelloWorld from './components/HelloWorld'
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
+import OutraLista from './components/OutraLista'
 
 function App() {
-const nome = "Matheus"
+  const meusItens = ['React', 'Vue', 'Angular']
 
   return (
     <div className="App">
-      <HelloWorld />
-      <SayMyName nome="João" />
-      <SayMyName nome="Lucas" />
-      <SayMyName nome={nome} />
-      <Pessoa 
-      nome="Rodrigo" 
-      idade="28" 
-      profissao="Programador"
-      foto="https://imgs.search.brave.com/USbJAJlX1WjvvghOPHYrj_vaJonZFaE3Z-ep32Pl9iM/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTc3/Nzc3MzM4Ny9wdC9m/b3RvL21vdW50YWlu/cy1pbi10aGUtbW9y/bmluZy1vbi1hLWZv/Z2d5LWRheS5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9STBP/QUEtLVREd3lQUlpK/TUVSWVltdllLU2Rj/U2JqdjF1czR6LXFU/QXJYQT0"
-      />
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens} />
+      <OutraLista itens={[]} />
     </div>
-  );
+  )
 }
 
 export default App;
