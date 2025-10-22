a4a8b093bd47897598b24fc5d547cf0d26d2a1d4
ogabrieldias <gabrielbielbernardino@gmail.com>
2025-10-22 13:56:58 -0300

    Commit learn

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,17 +1,23 @@
-import './App.css'
-import OutraLista from './components/OutraLista'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
-  const meusItens = ['React', 'Vue', 'Angular']
-
   return (
-    <div className="App">
-      <h1>Renderização de Listas</h1>
-      <OutraLista itens={meusItens} />
-      <OutraLista itens={[]} />
-    </div>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/empresa" element={<Empresa />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
 export default App;
-
